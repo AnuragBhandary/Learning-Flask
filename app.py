1599a529b4e4106e998ee5e39ec10f90c8c5aa71
Anurag Bhandary <anuragb2901@gmail.com>
2023-12-21 23:19:29 +0000

    Add files via upload
    First mock webiste

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask
 
-app = Flasl(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello Anurag!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
